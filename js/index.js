a83272a03925fdbef0e7a39d37def387ede1c133
Megan Meyer <33827558+Megymegs@users.noreply.github.com>
2023-10-24 08:46:22 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,17 @@
-let currentPageURL = window.location.href;
-let text;
-
-function checkURL(currentPageURL, text) {  
-  currentPageURL = window.location.href;
-  text = "404";
-  console.log(currentPageURL);
-  if (currentPageURL.includes(text)) {
-    setTimeout(() => {
-      console.log(currentPageURL);
-      window.location.href = "/Assessments/"
-      return true;
-    }, 6800);
+document.addEventListener("DOMContentLoaded", function() {
+  let currentPageURL = window.location.href;
+  let text;
+  
+  function checkURL(currentPageURL, text) {  
+    currentPageURL = window.location.href;
+    text = "404";
+    console.log(currentPageURL);
+    if (currentPageURL.includes(text)) {
+      setTimeout(() => {
+        console.log(currentPageURL);
+        window.location.href = "/Assessments/"
+        return true;
+      }, 6800);
+    }
   }
-}
+});
