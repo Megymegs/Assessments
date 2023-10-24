5d35e087553112d95565579df5e92a3d38243ae6
Megan Meyer <33827558+Megymegs@users.noreply.github.com>
2023-10-24 08:48:04 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   let currentPageURL = window.location.href;
   let text;
+  console.log(currentPageURL);
   
   function checkURL(currentPageURL, text) {  
     currentPageURL = window.location.href;
