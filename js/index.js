ffb72511d2cf14a1dfb836dac5aa32e9bd9be7d6
Megan Meyer <33827558+Megymegs@users.noreply.github.com>
2023-10-24 08:52:41 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
   let currentPageURL = window.location.href;
   let text;
-  console.log(currentPageURL);
   
   function checkURL(currentPageURL, text) {  
     currentPageURL = window.location.href;
     text = "404";
-    console.log(currentPageURL);
     if (currentPageURL.includes(text)) {
       setTimeout(() => {
         console.log(currentPageURL);
